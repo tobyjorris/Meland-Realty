f581a7a07a4e0306d019bf0e1e2b2abc391169d8
Toby Jorris <tobyjorris@gmail.com>
2021-03-17 11:28:45 -0500

    slider logic working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import image1 from '../../assets/header-pic-1.jpg';
 import image2 from '../../assets/header-photo-2.png';
+import image3 from '../../assets/header-pic-3.jpg';
 import './Header.scss';
 
 const Header = () => {
     const images = [
-        image1, image2
+        image1, image2, image3
     ]
     const [index, setIndex] = useState(0);
 
-    const slideRight = () => {
-    setIndex((index + 1) % images.length); // increases index by 1
-        console.log(index)
-    };
-
-    setInterval(() => {
-        slideRight();
-    }, 8000)
+    useEffect(() => {
+        const sliderId = setInterval(() => (index < images.length - 1 ? setIndex(index + 1) : setIndex(0)), 8000);
+        return () => clearInterval(sliderId);
+    })
 
     return (
         <div className="Header">
@@ -24,6 +21,5 @@ const Header = () => {
         </div>
         )
 };
-
 
 export default Header;
